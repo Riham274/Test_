734a729ac5419119aad4c81a628b5064e2474eb7
Riham274 <riham.ararawi274@gmail.com>
2026-03-29 23:49:13 +0300

    second commit

--- Relations/Main.java
+++ Relations/Main.java
@@ -56,6 +56,12 @@ class Doctor {
 
 public class Main {
     public static void main(String[] args) {
+        // Inheritance
+        Dog dog = new Dog();
+        dog.print(); // Output: Dog is an Animal
+
+        // Composition
+        Human human = new Human();
 
     }
 }
