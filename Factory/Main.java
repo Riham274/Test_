1a0c63c92afafeebaa699445b555c709588c8d8d
Riham274 <riham.ararawi274@gmail.com>
2026-04-01 23:01:44 +0300

    simple factoty & factory method design pattern

---
+++ Factory/Main.java
@@ -0,0 +1,18 @@
+package Factory;
+
+public class Main {
+    public static void main(String[] args) {
+        // Simple Factory Pattern
+        Animal dog = AnimalFactory.createAnimal("dog");
+        dog.makeSound(); 
+
+        Animal cat = AnimalFactory.createAnimal("cat");
+        cat.makeSound();
+        
+        // Factory Method Pattern
+        AnimalCreator dogCreator = new DogCreator();
+        Animal dog2 = dogCreator.createAnimal();
+        dog2.makeSound();
+
+    }
+}
