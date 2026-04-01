c4127cc1ccceffc08a9a520348617f2a2916321d
Riham274 <riham.ararawi274@gmail.com>
2026-04-01 22:12:09 +0300

    Singleton Design Pattern

---
+++ Singleton/Single.java
@@ -0,0 +1,31 @@
+package Singleton;
+
+import java.util.List;
+
+public class Single {
+    //not a singleton class 
+    private Integer[] numbers = {1,2,3,4,5,6};
+    private List<Integer> list = List.of(numbers);
+
+    private static Single instance; // null
+    
+    private Single() {
+        System.out.println("Single class constructor called");
+    }
+    
+
+    public void printer() {
+        System.out.println("Numbers: " + list);
+    }
+
+    //
+    public Single getInstance() {
+        if (instance == null) {
+            instance = new Single();
+        }
+        return instance;
+    }
+
+}
+
+
